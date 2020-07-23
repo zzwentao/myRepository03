81a5be9246a596dffbabe5336d51a84816a7dafa
myGit <myGit@>
2020-07-24 00:04:34 +0800

    第一次提交HelloProject

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,5 +5,7 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
+         System.out.println("HelloWorld!");
+		  System.out.println("HelloWorld!");
     }
 }
